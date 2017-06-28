dee72e59aa341241d95882a08cc0402498645fdf
Riju <mailmeonriju@gmail.com>
2017-06-28 18:05:32 +0530

    start

---
+++ Hackerrank/30-days-challenge/linked-lists.py
@@ -0,0 +1,25 @@
+class Node:
+	def __init__(self,data):
+		self.data = data
+		self.next = None
+class Solution:
+	def display(self,head):
+		current = head
+		while current:
+			print current.data,
+			current = current.next	
+	def insert(self,head,data): 
+		node = Node(data)
+
+
+
+
+
+
+mylist= Solution()
+T=int(input())
+head=None
+for i in range(T):
+	data=int(input())
+	head=mylist.insert(head,data)    
+mylist.display(head);
