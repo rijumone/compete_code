0446c9fef9f7d8b55ea2ec5c14fc0b6a943b9bcd
Riju <mailmeonriju@gmail.com>
2017-08-03 03:45:01 +0530

    Points: 98.00 Rank: 75191

--- Hackerrank/data_structures/linked-lists/insert-a-node-at-the-tail-of-a-linked-list.py
+++ Hackerrank/data_structures/linked-lists/insert-a-node-at-the-tail-of-a-linked-list.py
@@ -1,3 +1,4 @@
+"""
 l = raw_input()
 mainL = []
 while l != "":
@@ -11,7 +12,32 @@ while l != "":
 	l = raw_input()
 
 
-
-
 # NULL, data = 2
 # 2 --> NULL, data = 3
+
+"""
+
+"""
+ Insert Node at the end of a linked list 
+ head pointer input could be None as well for empty list
+ Node is defined as
+"""
+class Node(object):
+
+	def __init__(self, data=None, next_node=None):
+	   self.data = data
+	   self.next = next_node
+
+	# return back the head of the linked list in the below method
+
+
+def Insert(head, data):
+	t = Node(data)
+	if head is None:
+		head = t
+	else:
+		l = head
+		while not l.next is None:
+			l = l.next
+		l.next = t
+	return head
