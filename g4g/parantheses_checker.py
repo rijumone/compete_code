c8980661d52b263df0d3d22dc37e5a515a4764bf
Riju <mailmeonriju@gmail.com>
2019-02-17 14:27:44 +0530

    p c complete

--- g4g/parantheses_checker.py
+++ g4g/parantheses_checker.py
@@ -1,11 +1,89 @@
-in_string = "(())"
 
-"""
+class Stack:
+	
+	stack = ""
+	
+	def __init___(self):
+		pass
+	
+	def push(self, element):
+		self.stack += element
+	
+	def pop(self):
+		element = None
+		if not self.is_empty():
+			element = self.stack[-1]
+		self.stack = self.stack[:-1]
+		return element
+
+	def __str__(self):
+		return self.stack
+
+	def is_empty(self):
+		if len(self.stack) <= 0:
+			return True
+		return False
+
+
+stack_obj = Stack()
+# print(stack_obj)
+# stack_obj.push('a')
+# stack_obj.push('b')
+# stack_obj.pop()
+# print(stack_obj)
+# stack_obj.pop()
+# stack_obj.pop()
+# print(stack_obj)
+
 closer_dct = {
 	"{": "}",	
 	"(": ")",	
 	"[": "]",	
 }
+
+
+def check_if_popped_match(starting_b, ending_b):
+	''' check if the passed popped bracket matches its
+	corresponding starting bracket'''
+	if closer_dct[starting_b] == ending_b:
+		return True
+	return True
+
+
+in_string = ""
+
+"""
+cases:
+
+}(()){}
+(()
+))))))
+
+"""
+u = False
+for element in in_string:
+	if element in ['{', '(', '[']:
+		stack_obj.push(element)
+	if element in ['}', ')', ']']:
+		if stack_obj.is_empty():
+			print('unbalanced')
+			u = True
+			break
+		popped_element = stack_obj.pop()
+		if not check_if_popped_match(popped_element, element):
+			print('unbalanced')
+			u = True
+
+if not stack_obj.is_empty():
+	print('unbalanced')
+	u = True
+
+if not u:
+	print('balanced')
+
+
+"""
+
 
 next_closer_str = ""
 
@@ -42,12 +120,3 @@ for i in range(len(in_string)):
 if not u:
 	print('balanced')
 """
-
-class Stack:
-	stack = ""
-	def __init___(self):
-		pass
-	def push(self, element):
-		pass
-	def pop(self, element):
-		pass
