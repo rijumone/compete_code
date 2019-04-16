969906ab59986b48cd97e966f96926061c076dec
Riju <mailmeonriju@gmail.com>
2019-04-17 00:45:48 +0530

    fix to m_p_bricks

--- Hackerearth/basic_programming/i-o/basics/m_p_bricks.py
+++ Hackerearth/basic_programming/i-o/basics/m_p_bricks.py
@@ -12,13 +12,15 @@ helper matrix
 +---+---+---+--------+----------+
 
 """
-total_bricks = 3694
+total_bricks = int(input())
+# total_bricks = 13
+# total_bricks = 4321
 
 # generate qudratic equation
 a = 1
 b = 1
 c = -1 * (total_bricks * 2) / 3
-
+# print(c)
 # import complex math module
 import cmath
 import math
@@ -30,17 +32,23 @@ d = (b**2) - (4*a*c)
 soln_lst = []
 soln_lst.append((-b-cmath.sqrt(d))/(2*a))
 soln_lst.append((-b+cmath.sqrt(d))/(2*a))
-
-n = math.ceil([_.real for _ in soln_lst if _.real >= 0][0])
-
+# print(soln_lst)
+_n = math.ceil([_.real for _ in soln_lst if _.real >= 0][0])
+# print(n)
 # now n is the number of rounds for m and p
+# import pdb; pdb.set_trace()
+# calculate the max total of bricks altogether possible
+max_total_bricks = (_n/2)*((3*_n)+3)
 
-# m is the number of bricks laid that round
-m = 3 + (n-1)*3
+# m is the upper limit of number of bricks laid that round
+m = 3 + (_n-1)*3
 
-# out of m bricks, n has been laid by P
+# deficit is the bricks we are falling short
+deficit = max_total_bricks - total_bricks
+# print(deficit)
 
-if m > n:
-	print('M')
+actual_bricks_laid_last_round = m - deficit
+if actual_bricks_laid_last_round <= _n:
+	print('Patlu')
 else:
-	print('P')
+	print('Motu')
