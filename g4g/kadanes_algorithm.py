49415fc8d71ea00d1fccad29ed31a5961c0d8c6a
Riju <riju@work-machine.hp>
2021-06-09 02:28:03 +0530

    added graph adjacency matrix

--- g4g/kadanes_algorithm.py
+++ g4g/kadanes_algorithm.py
@@ -5,8 +5,14 @@ max_ending_here = 0
 
 for item in in_lst:
 	max_ending_here += item
+	# if by adding current item to max_ending_here,
+	# negative integers are reached
+	# reset max_ending_here to 0
+	# we will start from the next item
 	if max_ending_here < 0:
 		max_ending_here = 0
+	# if max_ending_here exceeds max_yet,
+	# we have a new max_yet
 	if max_yet < max_ending_here:
 		max_yet = max_ending_here
 
