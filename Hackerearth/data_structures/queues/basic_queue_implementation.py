888f592bc7ea21fd2c1c8fbe40ec8a96be23f5af
Riju <riju@work-machine.hp>
2021-06-11 01:08:07 +0530

    Queue implemented with Python

--- Hackerearth/data_structures/queues/basic_queue_implementation.py
+++ Hackerearth/data_structures/queues/basic_queue_implementation.py
@@ -1,34 +1,64 @@
+'''
+FIFO
+'''
+
+class Node:
+    data, next = None, None
+    def __init__(self, value) -> None:
+        self.data = value
+    def __repr__(self):
+        return f'{self.data} -> {self.next}'
+
 class Queue:
-    def __init__(self):
-        self.items = []
-
-    def isEmpty(self):
-        return self.items == []
-
-    def enqueue(self, item):
-        self.items.insert(0,item)
-
-    def dequeue(self):
-        return self.items.pop()
-
-    def size(self):
-        return len(self.items)
+    # pointers
+    head = None # remove from head
+    tail = None # add to tail
     
-    def printQ(self):
-        return self.items
+    def is_empty(self):
+        if self.head:
+            return True
+        return False
 
+    def peek(self):
+        if self.head:
+            return self.head.data
+    
+    def add(self, value):
+        n = Node(value)
+        if self.tail:
+            # here the Node object is being set
+            self.tail.next = n
+        # update the tail pointer
+        self.tail = n
+        if not self.head:
+            # in case Queue is empty
+            self.head = n
 
+    def remove(self):
+        if not self.head:
+            return
+        data = self.head.data
+        self.head = self.head.next
+        if not self.head:
+            # in case Queue becomes empty
+            # update the tail pointer
+            self.tail = None
+        return data
 
-q = Queue()
+    def __repr__(self) -> str:
+        return str(self.head)
 
-q.enqueue(1)
-q.enqueue(3)
-q.enqueue(5)
-q.enqueue(7)
+if __name__ == '__main__':
+    q = Queue()
+    q.add(5)
+    q.add(6)
 
+    print(q)
+    print(q.peek())
+    print(q.remove())
+    print(q)
 
-print q.printQ()
+    q.add(7)
+    q.add(8)
 
-q.dequeue()
-
-print q.printQ()
+    print(q)
