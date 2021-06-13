0b63d438d8c5955ca3e496d4ec27317494b227dd
Riju <riju@work-machine.hp>
2021-06-13 15:57:50 +0530

    incomplete binary seatch

---
+++ Hackerearth/algorithms/binary-search.py
@@ -0,0 +1,19 @@
+def binary_search(low, high, key):
+    while low < high:
+        pass
+
+
+def main(lst, x):
+    print(lst)
+    n = len(lst)
+    # define and init low and high pointers
+    low = 0
+    high = n - 1
+
+
+if __name__ == '__main__':
+    # given a list,
+    # return index of x if exists
+    # else return -1
+    lst = [-2, -1, 5, 7, 13, 17, 18]
+    main(lst=lst, x=17)
