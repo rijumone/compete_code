57d13ce5fe85dacbe8f068ea56329114fbd3fa11
Riju <mailmeonriju@gmail.com>
2021-08-26 01:28:18 +0530

    two-sum.py

---
+++ LeetCode/two-sum.py
@@ -0,0 +1,18 @@
+"""
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+
+Ref: https://leetcode.com/problems/two-sum/
+"""
+
+def main(lst, target):
+    pass
+
+if __name__ == '__main__':
+    lst = [2, 7, 11, 15]
+    target = 9
+    # expected output = [0, 1]
+    main(lst, target)
