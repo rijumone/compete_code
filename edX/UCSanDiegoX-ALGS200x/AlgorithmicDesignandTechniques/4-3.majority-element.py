4d0218fdefee207bbb88f92e97bef6ce12a30d67
Rijumone Choudhuri <rijumone@pop-os.localdomain>
2022-07-09 18:28:34 +0530

    added more logs

--- edX/UCSanDiegoX-ALGS200x/AlgorithmicDesignandTechniques/4-3.majority-element.py
+++ edX/UCSanDiegoX-ALGS200x/AlgorithmicDesignandTechniques/4-3.majority-element.py
@@ -46,22 +46,27 @@ def divide_and_conquer(lst):
             lo (int): starting index
             hi (int): ending index
         '''
+        print(f'List received: {[lst[_] for _ in range(lo, hi+1,)]}')
         # tacking the base case first,
-        # the only element in an array of len = 1
+        # the only element in an array of len = 1,
         # is the majority element
         if lo == hi:
+            print(f'lo == hi; lst[lo] = {lst[lo]} ')
             return lst[lo]
 
         # DIVIDE lst into left and right halves
         # get their majority_elem()
         mid = (hi - lo) // 2 + lo
         left_m_e = majority_elem(lo, mid)
+        print(f'left_m_e = {left_m_e}')
         right_m_e = majority_elem(mid + 1, hi)
-        # logger.info(left_m_e)
+        print(f'right_m_e = {right_m_e}')
+
         # logger.info(right_m_e)
         # If majority_elem() is the same for both
         # left and right halves, return it
         if left_m_e == right_m_e:
+            print(f'left_m_e == right_m_e = {left_m_e}')
             return left_m_e
 
         # otherwise, count each element in the list
@@ -81,5 +86,6 @@ if __name__ == '__main__':
     # _o = brute_force('2 3 9 2 2'.split(' '))
     # _o = divide_and_conquer('2 3 9 2 2'.split(' '))
     # _o = divide_and_conquer('1 2 3 4'.split(' '))
-    _o = divide_and_conquer('1 3 3 4 4 7 7 9 9 9'.split(' '))
+    _o = divide_and_conquer(
+        [int(_) for _ in '1 3 3 4 4 7 7 9 9 9'.split(' ')])
     print(_o)
