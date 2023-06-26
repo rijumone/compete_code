716713e894ee924bfca07c8df29e367d6a5a48ba
Rijumone Choudhuri <mailmeonriju@gmail.com>
2023-06-26 10:43:35 +0530

    Removed debug statements

--- cracking-the-coding-interview/trees/create_min_height_bst.py
+++ cracking-the-coding-interview/trees/create_min_height_bst.py
@@ -18,29 +18,18 @@ class Node:
 
 def main(lst, parent, parent_idx):
     ''''''
-    print(f'==== {lst}')
-    
-    print(f'parent_idx_value: {lst[parent_idx]}')
-    # pick left parent
-    
     lefts = lst[:parent_idx]
     if lefts:
         left_idx = int(len(lefts)/2)
-        print(f'lefts: {lefts}')
-        print(f'left_idx_value: {lefts[left_idx]}')
         parent.left = Node(lefts[left_idx])
         main(lefts, parent.left, parent_idx=int(len(lefts)/2))
     
-    print(f'lst: {lst}')
     rights = lst[parent_idx+1:]
     if rights:
         right_idx = int(len(rights)/2)
-        print(f'rights: {rights}')
-        print(f'right_idx_value: {rights[right_idx]}')
         parent.right = Node(rights[right_idx])
         main(rights, parent.right, parent_idx=int(len(rights)/2))
     
-    print(f'parent: {parent}')
     return parent
     
 
@@ -49,4 +38,4 @@ if __name__ == '__main__':
     parent_idx = int(len(lst)/2)
     parent = Node(lst[parent_idx])
     parent = main(lst, parent, parent_idx=int(len(lst)/2))
-    # import pdb;pdb.set_trace()
+    print(parent)
